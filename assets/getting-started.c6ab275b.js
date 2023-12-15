10e70fe8ce44edc4e47955390de5531224205f54
iqbal-rashed <iqbal-rashed@users.noreply.github.com>
2023-12-15 12:23:49 +0000

    deploy: e737bf9ea9a02b873f49cfa06467a42654bc62d8

---
+++ assets/getting-started.c6ab275b.js
@@ -0,0 +1,46 @@
+import{jsx as l,jsxs as n,Fragment as o}from"react/jsx-runtime";import{P as c,a,b as t,c as i}from"./index.b3eadc13.js";import"react";const y=void 0,F=[{id:"installation",text:"Installation",depth:2},{id:"basic-usage",text:"Basic Usage",depth:2}],E="Getting Started";function r(s){const e=Object.assign({h1:"h1",a:"a",p:"p",h2:"h2",div:"div",button:"button",span:"span",pre:"pre",code:"code"},s.components);return n(o,{children:[n(e.h1,{id:"getting-started",children:[l(e.a,{className:"header-anchor","aria-hidden":"true",href:"#getting-started",children:"#"}),"Getting Started"]}),`
+`,l(e.p,{children:"Currently supported by all browsers that support CSS clip paths and transforms (generally most evergreen browsers including mobile, but excluding IE). No dependencies."}),`
+`,n(e.h2,{id:"installation",children:[l(e.a,{className:"header-anchor","aria-hidden":"true",href:"#installation",children:"#"}),"Installation"]}),`
+`,n(e.div,{className:"language-sh",style:{backgroundColor:"#2e3440ff"},children:[l(e.button,{className:"copy"}),l(e.span,{className:"lang",children:"sh"}),l(e.pre,{children:n(e.code,{className:"",children:[l(e.span,{className:"line",children:l(e.span,{style:{color:"#D8DEE9FF"},children:"npm install react-peel"})}),`
+`,l(e.span,{className:"line"})]})})]}),`
+`,n(e.h2,{id:"basic-usage",children:[l(e.a,{className:"header-anchor","aria-hidden":"true",href:"#basic-usage",children:"#"}),"Basic Usage"]}),`
+`,l("div",{style:{padding:"20px"},children:n(c,{height:200,width:200,drag:!0,children:[l(a,{style:{backgroundColor:"#81afcb"}}),l(t,{style:{backgroundColor:"#a0c7df"}}),l(i,{style:{backgroundColor:"#688394"}})]})}),`
+`,n(e.div,{className:"language-jsx",style:{backgroundColor:"#2e3440ff"},children:[l(e.button,{className:"copy"}),l(e.span,{className:"lang",children:"jsx"}),l(e.pre,{children:n(e.code,{className:"",children:[n(e.span,{className:"line",children:[l(e.span,{style:{color:"#81A1C1"},children:"<div"}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#8FBCBB"},children:"style"}),l(e.span,{style:{color:"#81A1C1"},children:"={"}),l(e.span,{style:{color:"#ECEFF4"},children:"{"}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#88C0D0"},children:"padding"}),l(e.span,{style:{color:"#ECEFF4"},children:":"}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#ECEFF4"},children:'"'}),l(e.span,{style:{color:"#A3BE8C"},children:"20px"}),l(e.span,{style:{color:"#ECEFF4"},children:'"'}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#ECEFF4"},children:"}"}),l(e.span,{style:{color:"#81A1C1"},children:"}>"})]}),`
+`,n(e.span,{className:"line",children:[l(e.span,{style:{color:"#D8DEE9FF"},children:"  "}),l(e.span,{style:{color:"#81A1C1"},children:"<"}),l(e.span,{style:{color:"#8FBCBB"},children:"PeelWrapper"}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#8FBCBB"},children:"height"}),l(e.span,{style:{color:"#81A1C1"},children:"={"}),l(e.span,{style:{color:"#B48EAD"},children:"200"}),l(e.span,{style:{color:"#81A1C1"},children:"}"}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#8FBCBB"},children:"width"}),l(e.span,{style:{color:"#81A1C1"},children:"={"}),l(e.span,{style:{color:"#B48EAD"},children:"200"}),l(e.span,{style:{color:"#81A1C1"},children:"}"}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#8FBCBB"},children:"drag"}),l(e.span,{style:{color:"#81A1C1"},children:">"})]}),`
+`,n(e.span,{className:"line",children:[l(e.span,{style:{color:"#D8DEE9FF"},children:"    "}),l(e.span,{style:{color:"#81A1C1"},children:"<"}),l(e.span,{style:{color:"#8FBCBB"},children:"PeelTop"}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#8FBCBB"},children:"style"}),l(e.span,{style:{color:"#81A1C1"},children:"={"}),l(e.span,{style:{color:"#ECEFF4"},children:"{"}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#88C0D0"},children:"backgroundColor"}),l(e.span,{style:{color:"#ECEFF4"},children:":"}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#ECEFF4"},children:'"'}),l(e.span,{style:{color:"#A3BE8C"},children:"#81afcb"}),l(e.span,{style:{color:"#ECEFF4"},children:'"'}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#ECEFF4"},children:"}"}),l(e.span,{style:{color:"#81A1C1"},children:"}></"}),l(e.span,{style:{color:"#8FBCBB"},children:"PeelTop"}),l(e.span,{style:{color:"#81A1C1"},children:">"})]}),`
+`,n(e.span,{className:"line",children:[l(e.span,{style:{color:"#D8DEE9FF"},children:"    "}),l(e.span,{style:{color:"#81A1C1"},children:"<"}),l(e.span,{style:{color:"#8FBCBB"},children:"PeelBack"}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#8FBCBB"},children:"style"}),l(e.span,{style:{color:"#81A1C1"},children:"={"}),l(e.span,{style:{color:"#ECEFF4"},children:"{"}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#88C0D0"},children:"backgroundColor"}),l(e.span,{style:{color:"#ECEFF4"},children:":"}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#ECEFF4"},children:'"'}),l(e.span,{style:{color:"#A3BE8C"},children:"#a0c7df"}),l(e.span,{style:{color:"#ECEFF4"},children:'"'}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#ECEFF4"},children:"}"}),l(e.span,{style:{color:"#81A1C1"},children:"}></"}),l(e.span,{style:{color:"#8FBCBB"},children:"PeelBack"}),l(e.span,{style:{color:"#81A1C1"},children:">"})]}),`
+`,n(e.span,{className:"line",children:[l(e.span,{style:{color:"#D8DEE9FF"},children:"    "}),l(e.span,{style:{color:"#81A1C1"},children:"<"}),l(e.span,{style:{color:"#8FBCBB"},children:"PeelBottom"}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#8FBCBB"},children:"style"}),l(e.span,{style:{color:"#81A1C1"},children:"={"}),l(e.span,{style:{color:"#ECEFF4"},children:"{"}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#88C0D0"},children:"backgroundColor"}),l(e.span,{style:{color:"#ECEFF4"},children:":"}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#ECEFF4"},children:'"'}),l(e.span,{style:{color:"#A3BE8C"},children:"#688394"}),l(e.span,{style:{color:"#ECEFF4"},children:'"'}),l(e.span,{style:{color:"#D8DEE9FF"},children:" "}),l(e.span,{style:{color:"#ECEFF4"},children:"}"}),l(e.span,{style:{color:"#81A1C1"},children:"}></"}),l(e.span,{style:{color:"#8FBCBB"},children:"PeelBottom"}),l(e.span,{style:{color:"#81A1C1"},children:">"})]}),`
+`,n(e.span,{className:"line",children:[l(e.span,{style:{color:"#D8DEE9FF"},children:"  "}),l(e.span,{style:{color:"#81A1C1"},children:"</"}),l(e.span,{style:{color:"#8FBCBB"},children:"PeelWrapper"}),l(e.span,{style:{color:"#81A1C1"},children:">"})]}),`
+`,l(e.span,{className:"line",children:l(e.span,{style:{color:"#81A1C1"},children:"</div>"})}),`
+`,l(e.span,{className:"line"})]})})]})]})}function C(s={}){const{wrapper:e}=s.components||{};return e?l(e,Object.assign({},s,{children:l(r,s)})):r(s)}const B="2023/12/15 12:22:56",D=`import { PeelBack, PeelBottom, PeelWrapper, PeelTop } from "../../src/index";
+
+# Getting Started
+
+Currently supported by all browsers that support CSS clip paths and transforms (generally most evergreen browsers including mobile, but excluding IE). No dependencies.
+
+## Installation
+
+\`\`\`sh
+npm install react-peel
+\`\`\`
+
+## Basic Usage
+
+<div style={{ padding: "20px" }}>
+  <PeelWrapper height={200} width={200} drag>
+    <PeelTop style={{ backgroundColor: "#81afcb" }}></PeelTop>
+    <PeelBack style={{ backgroundColor: "#a0c7df" }}></PeelBack>
+    <PeelBottom style={{ backgroundColor: "#688394" }}></PeelBottom>
+  </PeelWrapper>
+</div>
+
+\`\`\`jsx
+<div style={{ padding: "20px" }}>
+  <PeelWrapper height={200} width={200} drag>
+    <PeelTop style={{ backgroundColor: "#81afcb" }}></PeelTop>
+    <PeelBack style={{ backgroundColor: "#a0c7df" }}></PeelBack>
+    <PeelBottom style={{ backgroundColor: "#688394" }}></PeelBottom>
+  </PeelWrapper>
+</div>
+\`\`\`
+`;export{D as content,C as default,y as frontmatter,B as lastUpdatedTime,E as title,F as toc};
